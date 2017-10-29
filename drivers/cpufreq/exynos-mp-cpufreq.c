a52d2a7f74625ad24c51e75e7b0c36c39d43b45a
DarkLord1731 <darklord1731@gmail.com>
2017-10-29 21:18:04 +0530

    dvfs: separate min, max store function Functionality remains the same
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -911,13 +911,9 @@ static ssize_t show_cpufreq_min_limit(struct kobject *kobj,
 	return nsize;
 }
 
-static ssize_t store_cpufreq_min_limit(struct kobject *kobj, struct attribute *attr,
-					const char *buf, size_t count)
+static void save_cpufreq_min_limit(int input)
 {
-	int cluster1_input, cluster0_input;
-
-	if (!sscanf(buf, "%8d", &cluster1_input))
-		return -EINVAL;
+	int cluster1_input = input, cluster0_input;
 
 	if (cluster1_input >= (int)freq_min[CL_ONE]) {
 #ifdef CONFIG_SCHED_HMP
@@ -959,6 +955,17 @@ static ssize_t store_cpufreq_min_limit(struct kobject *kobj, struct attribute *a
 		pm_qos_update_request(&core_min_qos[CL_ONE], cluster1_input);
 	if (pm_qos_request_active(&core_min_qos[CL_ZERO]))
 		pm_qos_update_request(&core_min_qos[CL_ZERO], cluster0_input);
+}
+
+static ssize_t store_cpufreq_min_limit(struct kobject *kobj, struct attribute *attr,
+					const char *buf, size_t count)
+{
+	int cluster1_input;
+
+	if (!sscanf(buf, "%8d", &cluster1_input))
+		return -EINVAL;
+
+	save_cpufreq_min_limit(cluster1_input);
 
 	return count;
 }
@@ -990,13 +997,9 @@ static ssize_t show_cpufreq_max_limit(struct kobject *kobj,
 	return nsize;
 }
 
-static ssize_t store_cpufreq_max_limit(struct kobject *kobj, struct attribute *attr,
-					const char *buf, size_t count)
+static void save_cpufreq_max_limit(int input)
 {
-	int cluster1_input, cluster0_input;
-
-	if (!sscanf(buf, "%8d", &cluster1_input))
-		return -EINVAL;
+	int cluster1_input = input, cluster0_input;
 
 	if (cluster1_input >= (int)freq_min[CL_ONE]) {
 		if (cluster1_hotplugged) {
@@ -1032,6 +1035,17 @@ static ssize_t store_cpufreq_max_limit(struct kobject *kobj, struct attribute *a
 		pm_qos_update_request(&core_max_qos[CL_ONE], cluster1_input);
 	if (pm_qos_request_active(&core_max_qos[CL_ZERO]))
 		pm_qos_update_request(&core_max_qos[CL_ZERO], cluster0_input);
+}
+
+static ssize_t store_cpufreq_max_limit(struct kobject *kobj, struct attribute *attr,
+					const char *buf, size_t count)
+{
+	int cluster1_input;
+
+	if (!sscanf(buf, "%8d", &cluster1_input))
+		return -EINVAL;
+
+	save_cpufreq_max_limit(cluster1_input);
 
 	return count;
 }
