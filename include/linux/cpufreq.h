496b494790a7580dd3454ba7ef4879b4426acf4b
djb77 <dwayne.bakewell@gmail.com>
2018-02-05 00:57:45 +1100

    cpufreq: intelliminmax Governor
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>
    Signed-off-by: corsicanu <corsicanu22@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -499,6 +499,9 @@ extern struct cpufreq_governor cpufreq_gov_HYPER;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
+extern struct cpufreq_governor cpufreq_gov_intellimm;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellimm)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
 extern struct cpufreq_governor cpufreq_gov_powersave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
