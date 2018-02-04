a01401327c85e69c3fd6d38443291cdb330997c5
djb77 <dwayne.bakewell@gmail.com>
2018-02-05 00:58:04 +1100

    cpufreq: ondemandplus Governor
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>
    Signed-off-by: corsicanu <corsicanu22@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -523,6 +523,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
 extern struct cpufreq_governor cpufreq_gov_powersave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
