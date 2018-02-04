d917d943c13704288283bc2bdd797b70c6ade9ff
djb77 <dwayne.bakewell@gmail.com>
2018-02-05 00:57:53 +1100

    cpufreq: IRONACTIVE Governor
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>
    Signed-off-by: corsicanu <corsicanu22@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -514,6 +514,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
+extern struct cpufreq_governor cpufreq_gov_ironactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
 extern struct cpufreq_governor cpufreq_gov_powersave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
