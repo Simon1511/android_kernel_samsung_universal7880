45eb0a0e2c89063f4bdccc6baefbfb2df2a3431a
djb77 <dwayne.bakewell@gmail.com>
2018-07-18 13:43:23 +1000

    cpufreq: yankactive Governor
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>
    Signed-off-by: corsicanu <corsicanu22@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -562,6 +562,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #endif
 
 /*********************************************************************
