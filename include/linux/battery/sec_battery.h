c7607384f13eceba2f4a157ad45dceb5fa16a11e
Andrei F <https://github.com/AndreiLux>
2018-08-23 14:48:51 +0400

    sec Battery charge control for Samsung UNIVERSAL platforms

--- include/linux/battery/sec_battery.h
+++ include/linux/battery/sec_battery.h
@@ -447,6 +447,8 @@ enum {
 	FACTORY_MODE_BYPASS,
 };
 
+void charger_control_init(struct sec_battery_info *info);
+
 #ifdef CONFIG_OF
 extern int adc_read(struct sec_battery_info *battery, int channel);
 extern void adc_init(struct platform_device *pdev, struct sec_battery_info *battery);
