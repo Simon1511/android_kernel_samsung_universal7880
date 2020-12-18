3c6654bdfdda25980e45b9ddb396302b7d8c173f
Simon1511 <simon2002.schoenmackers@gmail.com>
2020-12-18 19:24:32 +0100

    Add building scripts

---
+++ build_a5.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+export ARCH=arm64
+export CROSS_COMPILE=/media/simon/Linux_data/android-build-tools/ubertc4/bin/aarch64-linux-android-
+export ANDROID_MAJOR_VERSION=p
+
+make rise-a5y17lte_defconfig
+make -j64
+
