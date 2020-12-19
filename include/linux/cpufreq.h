e12f63c28b8e2c0a237a86c2889104587849c5da
ryan-andri <ryanandri@linuxmail.org>
2020-12-19 15:33:38 +0100

    cpufreq: Add nightmare governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -493,6 +493,9 @@ extern unsigned int cpufreq_interactive_get_hispeed_freq(int cpu);
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
 extern struct cpufreq_governor cpufreq_gov_powersave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
