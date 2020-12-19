b745ed1abe7dd9a0fcd97f618daf43470d61ea58
Alucard24 <dmbaoh2@gmail.com>
2020-12-19 15:21:09 +0100

    cpufreq: Add alucard governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -508,6 +508,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_alucard)
 #endif
 
 /*********************************************************************
