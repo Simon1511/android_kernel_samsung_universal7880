9798a844902368b416785cbdd13e60611292470b
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-05-12 23:17:05 +0200

    rise: Add AIK for all versions -Also add a cleanup option to the build script
    Signed-off-by: Simon1511 <simon2002.schoenmackers@gmail.com>

--- build_rise.sh
+++ build_rise.sh
@@ -2,6 +2,9 @@
 
 # Set to y to see full ouput of make
 debug=n
+dtbpath=arch/arm64/boot/dtb.img
+imagepath=arch/arm64/boot/Image
+aikpath=rise/AIK
 
 BUILD_BOOT() {
     variant=$1
@@ -38,6 +41,7 @@ BUILD_BOOT() {
         fi
     fi
 
+    clear
     echo "Building..."
 
     if [[ "$debug" == "n" ]]; then
@@ -51,13 +55,34 @@ BUILD_BOOT() {
     elif [[ "$dev" == "a7" ]]; then
         rm arch/arm64/boot/dts/exynos7880-a7y17lte_common.dtsi
     fi
+
+    echo "Packing boot.img..."
+
+    if [[ "$variant" == "AOSP 10.0" ]]; then
+        cp $imagepath $aikpath/Lineage/split_img/boot.img-zImage
+        cp $dtbpath $aikpath/Lineage/split_img/boot.img-dt
+        chmod +x $aikpath/Lineage/repackimg.sh
+        $aikpath/Lineage/repackimg.sh
+    elif [[ "$variant" == "OneUI 10.0" ]]; then
+        cp $imagepath $aikpath/OneUI/split_img/boot.img-zImage
+        cp $dtbpath $aikpath/OneUI/split_img/boot.img-dt
+        chmod +x $aikpath/OneUI/repackimg.sh
+        $aikpath/OneUI/repackimg.sh
+    elif [[ "$variant" == "Treble 10.0" ]]; then
+        cp $imagepath $aikpath/Treble/split_img/boot.img-zImage
+        cp $dtbpath $aikpath/Treble/split_img/boot.img-dt
+        chmod +x $aikpath/Treble/repackimg.sh
+        $aikpath/Treble/repackimg.sh
+    fi
 }
 
+clear
 echo "Select build variant:"
 
 select opt in "AOSP 10.0" "Treble 10.0" "OneUI 10.0"
 do
 
+    clear
     echo "Selected: $opt"
 
     case $opt in
@@ -96,6 +121,13 @@ do
     esac
 done
 
+clear
 echo "Build finished"
 
+read -p "Clean build directory? [Y/n] " clean
+if [[ "$clean" = "Y" ]]; then
+    ./cleanup.sh > /dev/null 2>&1
+elif [[ "$clean" = "n" ]]; then
+    exit
+fi
 
