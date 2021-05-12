6159b1a472c3f4995bb18167dadc5bc3e73ca74e
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-05-12 19:44:02 +0200

    rise: Also export SUBARCH
    Signed-off-by: Simon1511 <simon2002.schoenmackers@gmail.com>

--- build_rise.sh
+++ build_rise.sh
@@ -8,6 +8,7 @@ BUILD_BOOT() {
     dev=$2
 
     export ARCH=arm64
+    export SUBARCH=arm64
     export CROSS_COMPILE=/media/simon/Linux_data/android-build-tools/ubertc4/bin/aarch64-linux-android-
     export ANDROID_MAJOR_VERSION=p
 
