648316ffaa024226b209c651e114beb88743a6f5
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-07-27 11:29:50 +0200

    rise: zip: Use 7z if installed
    Signed-off-by: Simon1511 <simon2002.schoenmackers@gmail.com>

--- rise/zip/zip.sh
+++ rise/zip/zip.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 riseVer=$1
 buildDate=$2
+
+# Colors
+RED='\033[0;31m'
+NC='\033[0m'
 
 if [[ ! `echo $PWD` == *"rise/zip"* ]]; then
     cd rise/zip
@@ -11,9 +17,14 @@ cat META-INF/com/google/android/update-binary > update-binary.bak
 
 sed -i "s|PLACEHOLDER|'$riseVer'|g" META-INF/com/google/android/update-binary
 
-
-zip -r9 riseKernel-$riseVer-$buildDate-a57y17lte.zip META-INF/ rise/*/*.img
-
+# Use 7z if available
+if [[ `which 7z` == *"7z"* ]]; then
+    printf "\nCreating flashable zip using ${RED}7z${NC}..."
+    7z a -mmt16 -x'!rise/*/PLACEHOLDER' -tzip riseKernel-$riseVer-$buildDate-a57y17lte.zip META-INF/ rise/*/*.img
+else
+    printf "\nCreating flashable zip using ${RED}zip${NC}..."
+    zip -r9 riseKernel-$riseVer-$buildDate-a57y17lte.zip META-INF/ rise/*/*.img
+fi
 
 cat update-binary.bak > META-INF/com/google/android/update-binary
 rm update-binary.bak
