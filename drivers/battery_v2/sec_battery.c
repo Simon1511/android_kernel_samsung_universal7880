77774498a10ba1d5a93f46fa0114ed4d2c79a212
filiprrs <filiprrs@gmail.com>
2022-11-12 11:54:12 +0100

    sec_battery: Adjust storemode values for optimal battery life
    Change-Id: I1b3b20fad4d5965294649af3ac187cc4da6ce500

--- drivers/battery_v2/sec_battery.c
+++ drivers/battery_v2/sec_battery.c
@@ -17,7 +17,7 @@ static int wl_polling = 10;
 module_param(wl_polling, int, 0644);
 
 static unsigned int STORE_MODE_CHARGING_MAX = 85;
-static unsigned int STORE_MODE_CHARGING_MIN = 70;
+static unsigned int STORE_MODE_CHARGING_MIN = 15;
 
 module_param_named(store_mode_max, STORE_MODE_CHARGING_MAX, uint, S_IWUSR | S_IRUGO);
 module_param_named(store_mode_min, STORE_MODE_CHARGING_MIN, uint, S_IWUSR | S_IRUGO);
